40112b9c9fd4a70a06cce55991172483bb287028
root <xichao2014@gmail.com>
2018-03-30 02:27:43 +0000

    Hello world git program

---
+++ Hello.java
@@ -0,0 +1,12 @@
+public class Hello{
+
+	public static void main(String[] args)
+	{   
+		for(int i=1; i<=10; i++)
+		{
+		  System.out.println("Ana Taylor Joy ..."+i);
+		}  
+	}
+}
+// javac Hello.java to compile the file
+//java Hello to run this file
